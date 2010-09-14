0e95ef5dfde6eb20a063893e81c8841896e18867
Thach Tran <tranngocthachs@gmail.com>
2010-09-14 19:31:24 +0100

    minor change

--- src/dummy/model/actions/Test.java
+++ src/dummy/model/actions/Test.java
@@ -1,19 +1,10 @@
 package dummy.model.actions;
 
-import java.io.File;
-import java.io.FileInputStream;
-
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.mime.HttpMultipartMode;
-import org.apache.http.entity.mime.MultipartEntity;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.InputStreamBody;
-import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
